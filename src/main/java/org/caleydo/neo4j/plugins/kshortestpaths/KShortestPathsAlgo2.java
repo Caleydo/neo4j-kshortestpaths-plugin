2642e68fabd803c1a6d4139ee8c3bd840f831ec1
Samuel Gratzl <samuel_gratzl@gmx.at>
2015-07-07 14:00:08 +0200

    changes for 2.2.3

--- src/main/java/org/caleydo/neo4j/plugins/kshortestpaths/KShortestPathsAlgo2.java
+++ src/main/java/org/caleydo/neo4j/plugins/kshortestpaths/KShortestPathsAlgo2.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
 import org.neo4j.function.Function;
-import org.neo4j.graphalgo.impl.path.ShortestPath;
+import org.neo4j.graphalgo.GraphAlgoFactory;
 import org.neo4j.graphalgo.impl.util.WeightedPathImpl;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Path;
@@ -38,7 +38,7 @@ public class KShortestPathsAlgo2 {
 		int checkedLength = -1;
 
         //first attempt: classic shortest path
-		for(Path path : new ShortestPath(maxLength, expander).findAllPaths(start, end)) {
+		for (Path path : GraphAlgoFactory.shortestPath(expander, maxLength).findAllPaths(start, end)) {
 			checkedLength = path.length(); //we have checked this length but may not accept it
 			path = mapper.apply(path);
 			debug("here",path)	;
@@ -68,7 +68,7 @@ public class KShortestPathsAlgo2 {
         //the shortest paths after all). We try with longer path length until we have enough:
         for (int depth = checkedLength + 1; depth <= maxLength && result.size() < k; depth++) {
         	debug("check depth: ",depth);
-        	for(Path path : new ShortestPath(depth, expander, Integer.MAX_VALUE, true).findAllPaths(start, end)) {
+			for (Path path : GraphAlgoFactory.pathsWithLength(expander, depth).findAllPaths(start, end)) {
         		path = mapper.apply(path);
         		if (!pathAccepter.accept(path)) {
     				debug("dimiss length "+depth,path);
