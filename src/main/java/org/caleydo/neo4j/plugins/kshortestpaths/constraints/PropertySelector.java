b4680e6196d597a049025b3ce9ff834a0d25636f
Samuel Gratzl <samuel_gratzl@gmx.at>
2015-08-26 14:08:04 +0200

    bugfixing if property not exists (Caleydo/pathfinder#59)

--- src/main/java/org/caleydo/neo4j/plugins/kshortestpaths/constraints/PropertySelector.java
+++ src/main/java/org/caleydo/neo4j/plugins/kshortestpaths/constraints/PropertySelector.java
@@ -4,7 +4,7 @@ import org.neo4j.graphdb.PropertyContainer;
 
 public class PropertySelector implements ISelector{
 	private final String property;
-	
+
 
 	public PropertySelector(String property) {
 		super();
@@ -13,14 +13,14 @@ public class PropertySelector implements ISelector{
 
 	@Override
 	public Object get(PropertyContainer container) {
-		return container.getProperty(property);
+		return container.hasProperty(property) ? container.getProperty(property) : null;
 	}
-	
+
 	@Override
 	public String toString() {
 		return " prop:"+property;
 	}
-	
+
 	@Override
 	public String toCypher(String var, boolean isNode) {
 		return var+"."+property;
